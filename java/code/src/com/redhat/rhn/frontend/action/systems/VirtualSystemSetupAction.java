2fb98d8c9ba9e732e991b764c1ecb653627b8524
Eric Herget <eherget@redhat.com>
2016-10-14 15:42:03 -0400

    draft commit 3

--- java/code/src/com/redhat/rhn/frontend/action/systems/VirtualSystemSetupAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/systems/VirtualSystemSetupAction.java
@@ -15,15 +15,22 @@
 package com.redhat.rhn.frontend.action.systems;
 
 import com.redhat.rhn.common.db.datasource.DataResult;
+import com.redhat.rhn.domain.rhnset.RhnSet;
 import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.frontend.dto.SystemOverview;
 import com.redhat.rhn.frontend.dto.VirtualSystemOverview;
+import com.redhat.rhn.frontend.listview.PageControl;
 import com.redhat.rhn.frontend.struts.RequestContext;
 import com.redhat.rhn.frontend.struts.RhnAction;
+import com.redhat.rhn.frontend.struts.RhnHelper;
+import com.redhat.rhn.frontend.taglibs.list.ListTagHelper;
+import com.redhat.rhn.frontend.taglibs.list.TagHelper;
 import com.redhat.rhn.frontend.taglibs.list.helper.ListRhnSetHelper;
 import com.redhat.rhn.frontend.taglibs.list.helper.Listable;
 import com.redhat.rhn.manager.rhnset.RhnSetDecl;
 import com.redhat.rhn.manager.system.SystemManager;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -38,7 +45,10 @@ import javax.servlet.http.HttpServletResponse;
  * VirtualSystemsListSetupAction
  * @version $Rev$
  */
-public class VirtualSystemSetupAction extends RhnAction implements Listable {
+public class VirtualSystemSetupAction extends RhnAction
+        implements Listable<SystemOverview> {
+
+    private static final Logger LOG = Logger.getLogger(VirtualSystemSetupAction.class);
 
     /** {@inheritDoc} */
     @Override
@@ -46,11 +56,41 @@ public class VirtualSystemSetupAction extends RhnAction implements Listable {
             ActionForm formIn,
             HttpServletRequest request,
             HttpServletResponse response) {
+
+        RequestContext requestContext = new RequestContext(request);
+
+        User user = requestContext.getCurrentUser();
+        DataResult<VirtualSystemOverview> result = getDataResult(user, null, formIn);
+
+        for (VirtualSystemOverview vso : result) {
+            LOG.error("RESULT: " + vso.getName() + "   " + vso.getId() + "  " +
+                    vso.getServerName());
+        }
+        if (result.isEmpty()) {
+            request.setAttribute(BaseSystemsAction.SHOW_NO_SYSTEMS, Boolean.TRUE);
+        }
+
+        RhnSet set =  RhnSetDecl.SYSTEMS.get(user);
+
+
         ListRhnSetHelper helper =
-                new ListRhnSetHelper(this, request, RhnSetDecl.SYSTEM_GROUPS);
+                new ListRhnSetHelper(this, request, RhnSetDecl.SYSTEMS);
         helper.execute();
 
-        return mapping.findForward("default");
+        if (!set.isEmpty()) {
+            //helper.syncSelections(set, result);
+            ListTagHelper.setSelectedAmount("systemList", set.size(), request);
+        }
+
+
+        ListTagHelper.bindSetDeclTo("systemList", RhnSetDecl.SYSTEMS, request);
+
+        request.setAttribute(RequestContext.PAGE_LIST, result);
+        request.setAttribute(ListTagHelper.PARENT_URL, request.getRequestURI());
+
+        TagHelper.bindElaboratorTo("systemList", result.getElaborator(), request);
+
+        return mapping.findForward(RhnHelper.DEFAULT_FORWARD);
     }
 
     /**
@@ -75,6 +115,26 @@ public class VirtualSystemSetupAction extends RhnAction implements Listable {
         }
     }
 
+    protected DataResult<VirtualSystemOverview> getDataResult(User user, PageControl pc,
+            ActionForm formIn) {
+        DataResult<VirtualSystemOverview> dr = SystemManager.virtualSystemsList(user, pc);
+
+        for (int i = 0; i < dr.size(); i++) {
+            VirtualSystemOverview current = (VirtualSystemOverview) dr.get(i);
+            if (current.isFakeNode()) {
+                continue;
+            }
+            else if (current.getUuid() == null && current.getHostSystemId() != null) {
+                current.setSystemId(current.getHostSystemId());
+            }
+            else {
+                current.setSystemId(current.getVirtualSystemId());
+            }
+        }
+
+        return dr;
+    }
+
     @Override
     public List getResult(RequestContext context) {
         User user = context.getCurrentUser();
@@ -92,7 +152,6 @@ public class VirtualSystemSetupAction extends RhnAction implements Listable {
             }
         }
 
-        VirtualSystemOverview.processList(dr);
         return dr;
     }
 
